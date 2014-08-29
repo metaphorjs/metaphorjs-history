b25d582c6ab3ae8baf4d9cfea293e4250d52237f
kuindji <kuindji@gmail.com>
2014-08-29 16:26:57 +0400

    npm

---
+++ dist/metaphorjs.history.npm.js
@@ -0,0 +1,535 @@
+var Observable = require('metaphorjs-observable');
+
+var addListener = function(el, event, func) {
+    if (el.attachEvent) {
+        el.attachEvent('on' + event, func);
+    } else {
+        el.addEventListener(event, func, false);
+    }
+};
+var returnFalse = function() {
+    return false;
+};
+
+var returnTrue = function() {
+    return true;
+};
+var strUndef = "undefined";
+
+
+var isUndefined = function(any) {
+    return typeof any == strUndef;
+};
+
+var isNull = function(value) {
+    return value === null;
+};
+
+
+// from jQuery
+
+var NormalizedEvent = function(src) {
+
+    if (src instanceof NormalizedEvent) {
+        return src;
+    }
+
+    // Allow instantiation without the 'new' keyword
+    if (!(this instanceof NormalizedEvent)) {
+        return new NormalizedEvent(src);
+    }
+
+
+    var self    = this;
+
+    for (var i in src) {
+        if (!self[i]) {
+            try {
+                self[i] = src[i];
+            }
+            catch (thrownError){}
+        }
+    }
+
+
+    // Event object
+    self.originalEvent = src;
+    self.type = src.type;
+
+    if (!self.target && src.srcElement) {
+        self.target = src.srcElement;
+    }
+
+
+    var eventDoc, doc, body,
+        button = src.button;
+
+    // Calculate pageX/Y if missing and clientX/Y available
+    if (isUndefined(self.pageX) && !isNull(src.clientX)) {
+        eventDoc = self.target ? self.target.ownerDocument || document : document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
+
+        self.pageX = src.clientX +
+                      ( doc && doc.scrollLeft || body && body.scrollLeft || 0 ) -
+                      ( doc && doc.clientLeft || body && body.clientLeft || 0 );
+        self.pageY = src.clientY +
+                      ( doc && doc.scrollTop  || body && body.scrollTop  || 0 ) -
+                      ( doc && doc.clientTop  || body && body.clientTop  || 0 );
+    }
+
+    // Add which for click: 1 === left; 2 === middle; 3 === right
+    // Note: button is not normalized, so don't use it
+    if ( !self.which && button !== undefined ) {
+        self.which = ( button & 1 ? 1 : ( button & 2 ? 3 : ( button & 4 ? 2 : 0 ) ) );
+    }
+
+    // Events bubbling up the document may have been marked as prevented
+    // by a handler lower down the tree; reflect the correct value.
+    self.isDefaultPrevented = src.defaultPrevented ||
+                              isUndefined(src.defaultPrevented) &&
+                                  // Support: Android<4.0
+                              src.returnValue === false ?
+                              returnTrue :
+                              returnFalse;
+
+
+    // Create a timestamp if incoming event doesn't have one
+    self.timeStamp = src && src.timeStamp || (new Date).getTime();
+};
+
+// Event is based on DOM3 Events as specified by the ECMAScript Language Binding
+// http://www.w3.org/TR/2003/WD-DOM-Level-3-Events-20030331/ecma-script-binding.html
+NormalizedEvent.prototype = {
+
+    isDefaultPrevented: returnFalse,
+    isPropagationStopped: returnFalse,
+    isImmediatePropagationStopped: returnFalse,
+
+    preventDefault: function() {
+        var e = this.originalEvent;
+
+        this.isDefaultPrevented = returnTrue;
+        e.returnValue = false;
+
+        if ( e && e.preventDefault ) {
+            e.preventDefault();
+        }
+    },
+    stopPropagation: function() {
+        var e = this.originalEvent;
+
+        this.isPropagationStopped = returnTrue;
+
+        if ( e && e.stopPropagation ) {
+            e.stopPropagation();
+        }
+    },
+    stopImmediatePropagation: function() {
+        var e = this.originalEvent;
+
+        this.isImmediatePropagationStopped = returnTrue;
+
+        if ( e && e.stopImmediatePropagation ) {
+            e.stopImmediatePropagation();
+        }
+
+        this.stopPropagation();
+    }
+};
+
+
+
+var normalizeEvent = function(originalEvent) {
+    return new NormalizedEvent(originalEvent);
+};
+
+
+var slice = Array.prototype.slice;
+/**
+ * @param {*} obj
+ * @returns {boolean}
+ */
+var isPlainObject = function(obj) {
+    return !!(obj && obj.constructor === Object);
+};
+
+var isBool = function(value) {
+    return typeof value == "boolean";
+};
+
+
+/**
+ * @param {Object} dst
+ * @param {Object} src
+ * @param {Object} src2 ... srcN
+ * @param {boolean} override = false
+ * @param {boolean} deep = false
+ * @returns {*}
+ */
+var extend = function extend() {
+
+
+    var override    = false,
+        deep        = false,
+        args        = slice.call(arguments),
+        dst         = args.shift(),
+        src,
+        k,
+        value;
+
+    if (isBool(args[args.length - 1])) {
+        override    = args.pop();
+    }
+    if (isBool(args[args.length - 1])) {
+        deep        = override;
+        override    = args.pop();
+    }
+
+    while (args.length) {
+        if (src = args.shift()) {
+            for (k in src) {
+
+                if (src.hasOwnProperty(k) && !isUndefined((value = src[k]))) {
+
+                    if (deep) {
+                        if (dst[k] && isPlainObject(dst[k]) && isPlainObject(value)) {
+                            extend(dst[k], value, override, deep);
+                        }
+                        else {
+                            if (override === true || isUndefined(dst[k]) || isNull(dst[k])) {
+                                if (isPlainObject(value)) {
+                                    dst[k] = {};
+                                    extend(dst[k], value, override, true);
+                                }
+                                else {
+                                    dst[k] = value;
+                                }
+                            }
+                        }
+                    }
+                    else {
+                        if (override === true || isUndefined(dst[k]) || isNull(dst[k])) {
+                            dst[k] = value;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return dst;
+};
+
+
+
+var emptyFn = function(){};
+
+
+module.exports = function(){
+
+    var win             = window,
+        history         = win.history,
+        location        = win.location,
+        observable      = new Observable,
+        exports         = {},
+
+        listeners       = {
+            locationChange: [],
+            beforeLocationChange: []
+        },
+        windowLoaded    = false,
+        rURL            = /(?:(\w+:))?(?:\/\/(?:[^@]*@)?([^\/:\?#]+)(?::([0-9]+))?)?([^\?#]*)(?:(\?[^#]+)|\?)?(?:(#.*))?/,
+
+        pushStateSupported  = !!history.pushState,
+        hashChangeSupported = "onhashchange" in win;
+
+    extend(exports, observable.getApi());
+
+    var preparePath = function(url) {
+
+        var base = location.protocol + '//' + location.hostname;
+        if (location.port) {
+            base += ':' + location.port;
+        }
+
+        url = url.replace(base, '');
+
+        if (!pushStateSupported) {
+            url = encodeURIComponent(url);
+        }
+
+        return url;
+    };
+
+    var sameHostLink = function(url) {
+
+        var matches = url.match(rURL);
+
+        if (matches[1] && location.protocol != matches[1]) {
+            return false;
+        }
+
+        if (matches[2] && location.hostname != matches[2]) {
+            return false;
+        }
+
+        if (!matches[2] && !matches[3]) {
+            return true;
+        }
+
+        return location.port == matches[3];
+    };
+
+    var getPathFromUrl  = function(url) {
+
+        url = "" + url;
+
+        var matches = url.match(rURL),
+            path,
+            hash;
+
+        if (!pushStateSupported) {
+            hash    = matches[6];
+            if (hash.substr(0,1) == "!") {
+                path    = hash.substr(1);
+            }
+        }
+
+        if (!path) {
+            path    = matches[4];
+
+            if (matches[5]) {
+                path    += "?" + matches[5];
+            }
+        }
+
+        return path;
+    };
+
+    var samePathLink = function(url) {
+        return getPathFromUrl(url) == getPathFromUrl(location);
+    };
+
+    var setHash = function(hash) {
+        if (hash) {
+            location.hash = "!" + hash;
+        }
+        else {
+            location.hash = "";
+        }
+    };
+
+    var getCurrentUrl = function() {
+        var loc;
+
+        if (pushStateSupported) {
+            loc = location.pathname + location.search;
+        }
+        else {
+            loc = location.hash.substr(1);
+
+            if (loc) {
+                if (loc.substr(0, 1) == "!") {
+                    loc = loc.substr(1);
+                }
+                loc = decodeURIComponent(loc);
+            }
+            else {
+                loc = location.pathname + location.search;
+            }
+        }
+
+        return loc;
+    };
+
+    var triggerEvent = function triggerEvent(event, breakable, data) {
+
+        var url = data || getCurrentUrl(),
+            res;
+
+        for (var i = -1, l = listeners[event].length; ++i < l;){
+            res = listeners[event][i].call(null, url);
+            if (breakable && res === false) {
+                return false;
+            }
+        }
+
+        return exports.trigger(event, url);
+    };
+
+    var init = function() {
+
+        // normal pushState
+        if (pushStateSupported) {
+
+            history.origPushState       = history.pushState;
+            history.origReplaceState    = history.replaceState;
+
+            addListener(win, "popstate", function(){
+                triggerEvent("locationChange");
+            });
+
+            history.pushState = function(state, title, url) {
+                if (triggerEvent("beforeLocationChange", true, url) === false) {
+                    return false;
+                }
+                history.origPushState(state, title, preparePath(url));
+                triggerEvent("locationChange");
+            };
+
+            history.replaceState = function(state, title, url) {
+                if (triggerEvent("beforeLocationChange", true, url) === false) {
+                    return false;
+                }
+                history.origReplaceState(state, title, preparePath(url));
+                triggerEvent("locationChange");
+            };
+        }
+        else {
+
+            // onhashchange
+            if (hashChangeSupported) {
+
+                history.replaceState = history.pushState = function(state, title, url) {
+                    if (triggerEvent("beforeLocationChange", true, url) === false) {
+                        return false;
+                    }
+                    setHash(preparePath(url));
+                };
+                addListener(win, "hashchange", function(){
+                    triggerEvent("locationChange");
+                });
+            }
+            // iframe
+            else {
+
+                var frame   = null,
+                    initialUpdate = false;
+
+                var createFrame = function() {
+                    frame   = document.createElement("iframe");
+                    frame.src = 'about:blank';
+                    frame.style.display = 'none';
+                    document.body.appendChild(frame);
+                };
+
+                win.onIframeHistoryChange = function(val) {
+                    if (!initialUpdate) {
+                        setHash(val);
+                        triggerEvent("locationChange");
+                    }
+                };
+
+                var pushFrame = function(value) {
+                    var frameDoc = frame.contentWindow.document;
+                    frameDoc.open();
+                    //update iframe content to force new history record.
+                    frameDoc.write('<html><head><title>' + document.title +
+                                   '</title><script type="text/javascript">' +
+                                   'var hashValue = "'+value+'";'+
+                                   'window.top.onIframeHistoryChange(hashValue);' +
+                                   '</script>' +
+                                   '</head><body>&nbsp;</body></html>'
+                    );
+                    frameDoc.close();
+                };
+
+                var replaceFrame = function(value) {
+                    frame.contentWindow.hashValue = value;
+                };
+
+
+                history.pushState = function(state, title, url) {
+                    if (triggerEvent("beforeLocationChange", true, url) === false) {
+                        return false;
+                    }
+                    pushFrame(preparePath(url));
+                };
+
+                history.replaceState = function(state, title, url) {
+                    if (triggerEvent("beforeLocationChange", true, url) === false) {
+                        return false;
+                    }
+                    replaceFrame(preparePath(url));
+                };
+
+                var initFrame = function(){
+                    createFrame();
+                    initialUpdate = true;
+                    pushFrame(preparePath(location.hash.substr(1)));
+                    initialUpdate = false;
+                };
+
+                if (windowLoaded) {
+                    initFrame();
+                }
+                else {
+                    addListener(win, "load", initFrame);
+                }
+            }
+        }
+
+
+
+        addListener(document.documentElement, "click", function(e) {
+
+            e = normalizeEvent(e || win.event);
+
+            var a = e.target,
+                href;
+
+            while (a && a.nodeName.toLowerCase() != "a") {
+                a = a.parentNode;
+            }
+
+            if (a) {
+
+                href = a.getAttribute("href");
+
+                if (href && href.substr(0,1) != "#" && !a.getAttribute("target") &&
+                    sameHostLink(href) && !samePathLink(href)) {
+
+                    history.pushState(null, null, getPathFromUrl(href));
+
+                    e.preventDefault();
+                    e.stopPropagation();
+                    return false;
+                }
+            }
+
+            return null;
+        });
+
+        history.initPushState = emptyFn;
+        exports.initPushState = emptyFn;
+    };
+
+    addListener(win, "load", function() {
+        windowLoaded = true;
+    });
+
+    history.initPushState = init;
+
+    history.onBeforeChange = function(fn) {
+        listeners.beforeLocationChange.push(fn);
+    };
+    history.onChange = function(fn) {
+        listeners.locationChange.push(fn);
+    };
+
+    exports.pushUrl = function(url) {
+        history.pushState(null, null, url);
+    };
+    exports.replaceUrl = function(url) {
+        history.replaceState(null, null, url);
+    };
+    exports.currentUrl = function() {
+        return getCurrentUrl();
+    };
+    exports.initPushState = function() {
+        return init();
+    };
+
+    return exports;
+}();
